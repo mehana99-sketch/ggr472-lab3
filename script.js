f7a9973e06a31b9310aafac52b5d6eff0dbb0fed
mehana_n <mehana99@gmail.com>
2026-03-08 19:39:19 -0400

    added ct layer with hover

--- script.js
+++ script.js
@@ -12,24 +12,24 @@ const map = new mapboxgl.Map({
 map.on('load', () => {
 
     // Add data sources
-    // Wasn't working before; must be within "map.on('load', () => {..." section
-    // Add an existing data source from a GeoJSON file
+    // Note: page must be published for links to work
     map.addSource('StrollTO-POI', {
         type: 'geojson',
-        data: 'https://mehana99-sketch.github.io/ggr472-lab2/data/StrollTO-POI.geojson'
-    // URL to your geojson file, taken from github pages
+        data: 'https://mehana99-sketch.github.io/ggr472-lab3/data/StrollTO-POI.geojson'
+        // URL to your geojson file, taken from github pages
     });
     map.addSource('BicycleParking-Indoor', {
         type: 'geojson',
-        data: 'https://mehana99-sketch.github.io/ggr472-lab2/data/BicycleParking-Indoor.geojson'
+        data: 'https://mehana99-sketch.github.io/ggr472-lab3/data/BicycleParking-Indoor.geojson'
     });
     map.addSource('BicycleParking-Outdoor', {
         type: 'geojson',
-        data: 'https://mehana99-sketch.github.io/ggr472-lab2/data/BicycleParking-Outdoor.geojson'
+        data: ' https://mehana99-sketch.github.io/ggr472-lab3/data/BicycleParking-Outdoor.geojson'
     });
     map.addSource('TorontoCT', {
         type: 'geojson',
-        data: 'https://mehana99-sketch.github.io/ggr472-lab2/data/TorontoCT.geojson'
+        data: ' https://mehana99-sketch.github.io/ggr472-lab3/data/TorontoCT.geojson',
+        generateID: true // Create a unique ID for each feature
     });
 
     // Visualize data sources
@@ -60,4 +60,39 @@ map.on('load', () => {
             'circle-color': '#018bfc'
         }
     });
+
+    // Add CT layer!
+    map.addLayer({
+        'id': 'TorontoCT',
+        'type': 'fill',
+        'source': 'TorontoCT',
+        'paint': {
+            'fill-color': '#c96bd5',
+            'fill-opacity': 0.1,
+            'fill-outline-color': 'white',
+            'fill-outline-thickness': 0.5
+        }
+    },
+        'POI-point');
+    // This separately displays the highlighted CT (initially not called)
+    map.addLayer({
+        'id': 'TorontoCT-hl', // Update id to represent highlighted layer
+        'type': 'fill',
+        'source': 'TorontoCT',
+        'paint': {
+            'fill-color': '#c96bd5',
+            'fill-opacity': 0.8, // Higher opacity
+            'fill-outline-color': 'white'
+        },
+        'filter': ['==', ['get', 'AREA_LONG_CODE'], ''] // Set an initial filter to return nothing
+        // AREA_LONG_CODE is a unique ID for CT
+    }, 'POI-point'); //layer is drawn immediately below POI (points are on top)
+})
+
+// Event to add opacity to CT when hovering
+map.on('mousemove', 'TorontoCT', (e) => {
+    map.setFilter(
+        'TorontoCT-hl',
+        ['==', ['get', 'AREA_LONG_CODE'], e.features[0].properties.AREA_LONG_CODE]
+    );
 });
